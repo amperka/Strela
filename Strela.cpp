d259b8687a667006000d02ae11098783b106f907
acosinwork <acosinwork@yandex.ru>
2014-05-22 17:06:44 +0400

    initial commit

---
+++ Strela.cpp
@@ -0,0 +1,284 @@
+/* Strela.cpp
+ * Strela library 
+ *
+ *
+ */
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <Arduino.h>
+#include <Wire.h> 
+
+#include <Strela.h>
+
+
+ISR(TIMER4_OVF_vect)          // interrupt service routine for software PWM
+{
+   PORTB |= _BV(5); //pin 9 HIGH
+
+}
+
+ISR(TIMER4_COMPD_vect)          // interrupt service routine for software PWM
+{
+    PORTB &= ~_BV(5); //pin 9 LOW
+
+}
+
+namespace {
+    
+    uint8_t lcdPinMode = 0;
+    uint8_t ledState = 0;
+    uint8_t lcdPinState =0;
+    
+    
+void twiPinMode(uint8_t address, uint8_t pinModeByte)
+{
+    Wire.begin();
+    Wire.beginTransmission(address);
+    Wire.write(WIRE_IO_CONFIG_MODE); // i/o config mode
+    Wire.write(pinModeByte); // 
+    Wire.endTransmission();    
+}
+    
+    
+void twiWriteOut(uint8_t address, uint8_t value)
+{
+    Wire.beginTransmission(address);
+    Wire.write(WIRE_OUTPUT_WRITE_MODE); // output write mode
+    Wire.write(~value);
+    Wire.endTransmission();
+}
+
+uint8_t twiReadIn(uint8_t address, uint8_t pin)
+{
+    
+    Wire.beginTransmission(address);
+    Wire.write(WIRE_INPUT_READ_MODE); // input read mode
+    Wire.endTransmission();
+ 
+    uint8_t n = Wire.requestFrom(address, (uint8_t)1);
+    // TODO: is reading without wait is right?
+    uint8_t state = Wire.read();
+    
+    return !(state & (1 << pin));
+}
+
+
+    
+}
+
+
+
+void uPinMode(uint8_t pin, uint8_t mode)
+{
+
+    if (pin < NUM_DIGITAL_PINS) // standart IO
+        {
+            pinMode( pin, mode );
+            return;
+        }
+        
+      
+    if ( pin > Q7)  //we can,t change pinMode on GPUX on addr 0x20 or more
+    {
+        return;
+    }
+    else 
+    {
+        if (mode != OUTPUT)
+            lcdPinMode |= 1 << ( _LC(pin) );
+        else
+            lcdPinMode &= ~(1 << ( _LC(pin) ));       
+         
+            twiPinMode(LCD_TWI_ADDR, lcdPinMode);
+    }                
+}
+
+
+void uDigitalWrite(uint8_t pin, uint8_t val)
+{
+    if (pin < NUM_DIGITAL_PINS) // standart IO
+    {
+        digitalWrite( pin, val );
+        return;
+    }
+    
+    if ( pin > LAST_PIN)  //NOT ON PIN
+    {
+        return;
+    }
+    
+    if ( pin > Q7)  //led or button
+    {
+        if ( pin > LS3 )
+        {
+            uint8_t curPin = pin - LS0;
+            (val) ? ledState |= 1 << curPin : ledState &= ~(1 << curPin);
+            twiWriteOut(GPUX_TWI_ADDR, ledState);
+        }
+        return;
+    }   
+    
+    else // pin>=Q0 && pin<=Q7
+    {
+        uint8_t curPin = _LC(pin);
+        (val) ? lcdPinState |= 1 << curPin : lcdPinState &= ~(1 << curPin);
+        twiWriteOut(LCD_TWI_ADDR, lcdPinState);
+    }        
+}
+
+uint8_t uDigitalRead(uint8_t pin)
+{
+    if (pin < NUM_DIGITAL_PINS) // standart IO
+        return digitalRead( pin );
+    
+    if ( pin > LAST_PIN)  //NOT ON PIN
+    {
+        return LOW;
+    }
+    
+    if ( pin > Q7)  //led or button
+    {
+        uint8_t curPin = pin - LS0;
+        return twiReadIn(GPUX_TWI_ADDR, curPin);
+    }   
+    
+    else // pin>=Q0 && pin<=Q7
+    {
+        uint8_t curPin = pin - Q0;
+        return twiReadIn(LCD_TWI_ADDR, curPin);
+    }        
+}
+
+
+
+
+void Strela::begin()
+{
+    // Variables initialize
+    
+    _currentMotorSpeed_1 = 0;
+    _currentMotorSpeed_2 = 0;
+    _pwmCounter = 0;
+    _motorConnection_1 = 0;
+    _motorConnection_2 = 0;
+
+    // Configure I2C i/o chip
+    twiPinMode(GPUX_TWI_ADDR, WIRE_IO_CONFIGURATION);
+    
+    // Configure I2C LCD i/o chip to OUTPUT
+    twiPinMode(LCD_TWI_ADDR, 0xff);
+
+    
+    // Configure Pin
+
+    DDRE |= _BV(2);           //pinMode(PE2, OUTPUT);
+    DDRD |= _BV(4);           //pinMode(4, OUTPUT);
+    DDRB |= _BV(5) | _BV(6);  //pinMode(9,10, OUTPUT);
+    
+    
+    // Configure software PWM4D Timer to fast PWM
+    TCCR4A |= (1<<COM4B1); //enable communicate pin 10 and OCR4
+    TCCR4D &= ~(1<<WGM41) & ~(1<<WGM40); 
+    TC4H =0;
+    
+}
+
+void Strela:: motorConnection(
+            bool direction_1,
+            bool direction_2)
+{
+    _motorConnection_1=direction_1;
+    _motorConnection_2=direction_2;    
+}
+
+void Strela::_setMotorDirections(
+            bool direction_1,
+            bool direction_2)
+{
+    bool direction = direction_1;
+    if (direction ^ _motorConnection_1)
+        PORTD |= _BV(4);  //digitalWrite(4, HIGH)
+    else
+        PORTD &= ~_BV(4); //digitalWrite(4, LOW)
+        
+    direction = direction_2;
+    if (direction ^ _motorConnection_2)
+        PORTE |= _BV(2);
+    else
+        PORTE &= ~_BV(2);
+        
+}
+
+void Strela::drive(
+            int motorSpeed_1,
+            int motorSpeed_2)
+{ 
+    _setMotorDirections((motorSpeed_1 < 0), (motorSpeed_2 < 0));
+
+    _currentMotorSpeed_1 = abs(motorSpeed_1);
+    _currentMotorSpeed_2 = abs(motorSpeed_2);
+    
+    //Motor 1 on PWM works on interrupts
+    
+    if (_currentMotorSpeed_1 >= 255)
+    {
+        TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D); //disable interrupts on TIMER4 OVF and OCR4D
+        PORTB |= _BV(5); //pin 9 HIGH
+    } 
+    else if (_currentMotorSpeed_1 == 0)
+    {
+        TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D);  //disable interrupts on TIMER4 OVF and OCR4D
+        PORTB &= ~_BV(5); //pin 9 LOW
+    }
+    else 
+    {
+        TCCR4C &= ~(1<<COM4D1);  //disable PWM on Pin 6
+ 
+        //set PWM period
+        OCR4D = _currentMotorSpeed_1;
+        //enable interrupts on TIMER4 OVF and OCR4D
+        TIMSK4 |= (1<<TOIE4)|(1<<OCIE4D);
+    }
+    
+    //Motor2 PWM is hardware 
+    
+    if (_currentMotorSpeed_2 >= 255)
+    {
+        //disable communicate pin 10 with OCR4 and PWM
+        TCCR4A &= ~(1<<PWM4B) & ~(1<<COM4B1);
+        PORTB |= _BV(6); //pin 10 HIGH
+    } 
+    else if (_currentMotorSpeed_2 == 0)
+    {
+        //disable communicate pin 10 with OCR4 and PWM
+        TCCR4A &= ~(1<<PWM4B) & ~(1<<COM4B1); 
+        PORTB &= ~_BV(6); //pin 10 LOW
+    }
+    else 
+    {   
+        //set PWM period
+        OCR4B = _currentMotorSpeed_2;
+        TCCR4A |= (1<<PWM4B)|(1<<COM4B1); //enable communicate pin 10 with OCR4 and enable PWM
+    }
+}
+
+
+bool Strela::buttonRead(uint8_t btn)
+{
+ 
+    return twiReadIn(GPUX_TWI_ADDR, (3 - btn));
+}
+
+void Strela::ledWrite(
+            uint8_t ld,
+            bool state)
+{
+    (state) ? ledState |= 1 << (7 - ld) : ledState &= ~(1 << (7 - ld));
+    twiWriteOut(GPUX_TWI_ADDR, ledState);
+
+}
+
+
---
+++ Strela.h
@@ -0,0 +1,166 @@
+/*
+ * Strela.h
+ *
+ *
+ */
+
+ 
+#ifndef STRELA_H
+#define STRELA_H
+
+#include <inttypes.h> 
+
+#define GPUX_TWI_ADDR            0x20
+#define LCD_TWI_ADDR             0x21
+#define WIRE_IO_CONFIG_MODE      0x03
+#define WIRE_IO_CONFIGURATION    0x0F
+#define WIRE_OUTPUT_WRITE_MODE   0x01
+#define WIRE_INPUT_READ_MODE     0x00
+
+
+// IO-pin defines
+
+#undef A11  //used with buzzer
+#undef A10  //used with en12
+#undef A9   //used with en34
+#undef A6   //used with dir34
+
+#define MOTOR_ENABLE_12_PIN      10
+#define MOTOR_ENABLE_34_PIN      9
+#define MOTOR_DIRECTION_34_PIN   4
+#define BUZZER                   12
+
+#define P1  11
+#define P2  13
+#define P3  5
+#define P4  6
+
+#define P5  A3
+#define P6  A2
+#define P7  A1
+#define P8  A0
+
+#define P9  8
+#define P11 A4
+#define P12 A5
+
+#define IR_PIN P12
+
+//I2C-expander IO pins
+
+#define P10 37 
+
+//LiquidCrystall
+//LCx вид (для LiquidCrystal_I2C.h)
+#define LC_ADDR    LCD_TWI_ADDR
+
+#define LC4 0
+#define LC5 1
+#define LC6 2
+#define LC7 3
+#define LCEN 4
+#define LCRW 5
+#define LCRS 6
+
+//Qx вид (для uSerialWrite())
+#define Q0 30 //NUM_DIGITAL_PINS 
+#define Q1 31
+#define Q2 32
+#define Q3 33
+#define Q4 34
+#define Q5 35
+#define Q6 36
+#define Q7 P10
+
+//Перевод из Qx в LCx
+#define _LC(q) ((q)-Q0)
+//Перевод из LCx в Qx
+#define _Q(x) ((x)+Q0)
+
+//LED and Buttons
+#define LS0 38
+#define LS1 39
+#define LS2 40
+#define LS3 41
+
+#define LS4 42
+#define LS5 43
+#define LS6 44
+#define LS7 45
+
+#define LAST_PIN LS7
+
+//Buttons
+#define S1 LS3
+#define S2 LS2
+#define S3 LS1
+#define S4 LS0
+//Leds
+#define L1 LS7
+#define L2 LS6
+#define L3 LS5
+#define L4 LS4
+
+//Перевод из Sx или Lx в LSx
+#define _LS_S(x) ((3-(x))+LS0)
+#define _LS_L(x) ((7-(x))+LS0)
+//Перевод из LSx в Sx или Lx
+#define _S_LS(x) ((3-((x)-LS0)
+#define _L_LS(x) ((7-((x)-LS0)
+
+
+//Motors
+#define M1 0
+#define M2 1
+
+
+void uPinMode(uint8_t pin, uint8_t mode);
+
+void uDigitalWrite(uint8_t pin, uint8_t val);
+
+uint8_t uDigitalRead(uint8_t pin);
+            
+
+class Strela
+{
+    public:
+        void begin();
+        
+        void drive(
+            int motorSpeed_1,
+            int motorSpeed_2);
+        
+        void motorConnection(
+            bool direction_1,
+            bool direction_2);
+            
+        void ledWrite(
+            uint8_t ld,
+            bool state);    
+            
+        bool buttonRead(uint8_t btn);
+        
+        void onTimerInterrupt();
+
+    
+    private:
+        
+        uint8_t _currentMotorSpeed_1;
+        uint8_t _currentMotorSpeed_2;
+        uint8_t _pwmCounter;
+        bool _motorConnection_1;
+        bool _motorConnection_2;
+        
+        void _setMotorDirections(
+            bool direction_1,
+            bool direction_2);
+            
+        void _setPWM9(uint8_t pwm);
+        void _setPWM10(uint8_t pwm);
+        
+        
+        
+};
+
+
+#endif
