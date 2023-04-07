1bc4464cd28fd48508d9c2ec2d635e1c28015a9c
GregoryDvornikov <114313542+Greengaraj@users.noreply.github.com>
2023-04-07 12:32:38 +0300

    fix library

--- Strela/Strela.cpp
+++ Strela/Strela.cpp
@@ -15,10 +15,10 @@
 #include <Strela.h>
 
 
-#define WIRE_IO_CONFIG_MODE      0x03
-#define WIRE_IO_CONFIGURATION    0x0F
-#define WIRE_OUTPUT_WRITE_MODE   0x01
-#define WIRE_INPUT_READ_MODE     0x00
+#define WIRE_IO_CONFIG_MODE     0x03
+#define WIRE_IO_CONFIGURATION   0x0F
+#define WIRE_OUTPUT_WRITE_MODE  0x01
+#define WIRE_INPUT_READ_MODE    0x00
 
 #define STRELA_INIT_CHECK if (!strelaInit) strelaInitialize()
 
@@ -29,9 +29,9 @@
 #undef A9   //used with en34
 #undef A6   //used with dir34
 
-#define MOTOR_ENABLE_12_PIN      10
-#define MOTOR_ENABLE_34_PIN      9
-#define MOTOR_DIRECTION_34_PIN   4
+#define MOTOR_ENABLE_12_PIN     10
+#define MOTOR_ENABLE_34_PIN     9
+#define MOTOR_DIRECTION_34_PIN  4
 
 #define LAST_PIN LS7
 
@@ -42,20 +42,15 @@
 
 
 
-ISR(TIMER4_OVF_vect)          // interrupt service routine for software PWM
-{
-   PORTB |= _BV(5); //pin 9 HIGH
-
+ISR(TIMER4_OVF_vect) {              // interrupt service routine for software PWM 
+   PORTB |= _BV(5);                 // pin 9 HIGH
 }
 
-ISR(TIMER4_COMPD_vect)          // interrupt service routine for software PWM
-{
-    PORTB &= ~_BV(5); //pin 9 LOW
-
+ISR(TIMER4_COMPD_vect) {            // interrupt service routine for software PWM
+    PORTB &= ~_BV(5);               // pin 9 LOW
 }
 
 namespace {
-    
     uint8_t lcdPinMode = 0;
     uint8_t ledState = 0;
     uint8_t lcdPinState = 0;
@@ -67,27 +62,19 @@ namespace {
     bool _motorConnection_2 = 0;
     
     bool strelaInit=false;
-    
-  
-    void _setMotorSpeed_1(int speed)
-    {
+
+    void _setMotorSpeed_1(int speed) {
         _currentMotorSpeed_1 = abs(speed);
     
         //Motor 1 on PWM works on interrupts
-    
-        if (_currentMotorSpeed_1 >= 255)
-        {
-            TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D); //disable interrupts on TIMER4 OVF and OCR4D
-            PORTB |= _BV(5); //pin 9 HIGH
-        } 
-        else if (_currentMotorSpeed_1 == 0)
-        {
-            TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D);  //disable interrupts on TIMER4 OVF and OCR4D
-            PORTB &= ~_BV(5); //pin 9 LOW
-        }
-        else 
-        {
-            TCCR4C &= ~(1<<COM4D1);  //disable PWM on Pin 6
+        if (_currentMotorSpeed_1 >= 255) {
+            TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D);   //disable interrupts on TIMER4 OVF and OCR4D
+            PORTB |= _BV(5);                        //pin 9 HIGH
+        }  else if (_currentMotorSpeed_1 == 0) {
+            TIMSK4 &= ~(1<<TOIE4) & ~(1<<OCIE4D);   //disable interrupts on TIMER4 OVF and OCR4D
+            PORTB &= ~_BV(5);                       //pin 9 LOW
+        } else  {
+            TCCR4C &= ~(1<<COM4D1);                 //disable PWM on Pin 6
  
             //set PWM period
             OCR4D = _currentMotorSpeed_1;
@@ -97,91 +84,70 @@ namespace {
     
     }
 
-    void _setMotorSpeed_2(int speed)
-    {
+    void _setMotorSpeed_2(int speed) {
         _currentMotorSpeed_2 = abs(speed);
     
         //Motor2 PWM is hardware 
-    
-        if (_currentMotorSpeed_2 >= 255)
-        {
+        if (_currentMotorSpeed_2 >= 255) {
             //disable communicate pin 10 with OCR4 and PWM
             TCCR4A &= ~(1<<PWM4B) & ~(1<<COM4B1);
             PORTB |= _BV(6); //pin 10 HIGH
-        } 
-        else if (_currentMotorSpeed_2 == 0)
-        {
+        } else if (_currentMotorSpeed_2 == 0) {
             //disable communicate pin 10 with OCR4 and PWM
             TCCR4A &= ~(1<<PWM4B) & ~(1<<COM4B1); 
             PORTB &= ~_BV(6); //pin 10 LOW
-        }
-        else 
-        {   
+        } else {
             //set PWM period
             OCR4B = _currentMotorSpeed_2;    
             TCCR4A |= (1<<PWM4B)|(1<<COM4B1); //enable communicate pin 10 with OCR4 and enable PWM
         }
     }
 
-    void _setMotorDirections(
-                bool direction_1,
-                bool direction_2)
-    {
+    void _setMotorDirections(bool direction_1, bool direction_2) {
         bool direction = direction_1;
         if (direction ^ _motorConnection_1)    
             PORTD |= _BV(4);  //digitalWrite(4, HIGH)
         else
             PORTD &= ~_BV(4); //digitalWrite(4, LOW)
-        
+
         direction = direction_2;
         if (direction ^ _motorConnection_2)
             PORTE |= _BV(2);
         else
             PORTE &= ~_BV(2);
-        
     }
 
-    void _setMotorDirection(
-                uint8_t mot,
-                bool direction)
-    {
-        if (mot == M1)
-        {
+    void _setMotorDirection(uint8_t mot, bool direction) {
+        if (mot == M1) {
             if (direction ^ _motorConnection_1)
                 PORTD |= _BV(4);  //digitalWrite(4, HIGH)
             else
                 PORTD &= ~_BV(4); //digitalWrite(4, LOW)
-        }
-        else if (mot == M2)
-       {    
+        } else if (mot == M2) {
             if (direction ^ _motorConnection_2)
                 PORTE |= _BV(2);
             else
                 PORTE &= ~_BV(2);
-        }    
+        }
     }
    
-    void twiPinMode(uint8_t address, uint8_t pinModeByte)
-    {
+    void twiPinMode(uint8_t address, uint8_t pinModeByte) {
         Wire.begin();
         Wire.beginTransmission(address);
-        Wire.write(WIRE_IO_CONFIG_MODE); // i/o config mode
-        Wire.write(pinModeByte); // 
-        Wire.endTransmission();    
+        Wire.write(WIRE_IO_CONFIG_MODE);    // i/o config mode
+        Wire.write(pinModeByte);            // 
+        Wire.endTransmission();
     }
-    
-    
-    void twiWriteOut(uint8_t address, uint8_t value)
-    {
+
+
+    void twiWriteOut(uint8_t address, uint8_t value) {
         Wire.beginTransmission(address);
         Wire.write(WIRE_OUTPUT_WRITE_MODE); // output write mode
         Wire.write(value);
         Wire.endTransmission();
     }
 
-    uint8_t twiReadIn(uint8_t address, uint8_t pin)
-    {
-    
+    uint8_t twiReadIn(uint8_t address, uint8_t pin) {
         Wire.beginTransmission(address);
         Wire.write(WIRE_INPUT_READ_MODE); // input read mode
         Wire.endTransmission();
@@ -193,231 +159,184 @@ namespace {
         return (bool)(state & (1 << pin));
     }
 
-
     /*
- * Moves the motor forward or backwards.
- */
-    void _stepMotor(int thisStep)
-    {
-  
+    * Moves the motor forward or backwards.
+    */
+    void _stepMotor(int thisStep) {
       switch (thisStep) {
         case 0:    // 1, 1, 0, 0
-        PORTB |= _BV(5);
-        PORTD |= _BV(4);
-        PORTB &= ~_BV(6);
-        PORTE &= ~_BV(2);
+            PORTB |= _BV(5);
+            PORTD |= _BV(4);
+            PORTB &= ~_BV(6);
+            PORTE &= ~_BV(2);
         break;
         case 1:    // 1, 1, 1, 1
-        PORTB |= _BV(5);
-        PORTD |= _BV(4);
-        PORTB |=_BV(6);
-        PORTE |= _BV(2);
+            PORTB |= _BV(5);
+            PORTD |= _BV(4);
+            PORTB |=_BV(6);
+            PORTE |= _BV(2);
         break;
         case 2:    //0, 0, 1, 1
-        PORTB &= ~_BV(5);
-        PORTD &= ~_BV(4);
-        PORTB |=_BV(6);
-        PORTE |= _BV(2);
+            PORTB &= ~_BV(5);
+            PORTD &= ~_BV(4);
+            PORTB |=_BV(6);
+            PORTE |= _BV(2);
         break;
         case 3:    //1, 0, 1, 1
-        PORTB |= _BV(5);
-        PORTD &= ~_BV(4);
-        PORTB |=_BV(6);
-        PORTE |= _BV(2);
+            PORTB |= _BV(5);
+            PORTD &= ~_BV(4);
+            PORTB |=_BV(6);
+            PORTE |= _BV(2);
         break;
-    
         case 4:    // 1, 0, 0, 0
-        PORTB |= _BV(5);
-        PORTD &= ~_BV(4);
-        PORTB &= ~_BV(6);
-        PORTE &= ~_BV(2);
+            PORTB |= _BV(5);
+            PORTD &= ~_BV(4);
+            PORTB &= ~_BV(6);
+            PORTE &= ~_BV(2);
         break;
         case 5:    // 1, 0, 1, 0
-        PORTB |= _BV(5);
-        PORTD &= ~_BV(4);
-        PORTB |=_BV(6);
-        PORTE &= ~_BV(2);
+            PORTB |= _BV(5);
+            PORTD &= ~_BV(4);
+            PORTB |=_BV(6);
+            PORTE &= ~_BV(2);
         break;
         case 6:    //0, 0, 1, 0
-        PORTB &= ~_BV(5);
-        PORTD &= ~_BV(4);
-        PORTB |=_BV(6);
-        PORTE &= ~_BV(2);
+            PORTB &= ~_BV(5);
+            PORTD &= ~_BV(4);
+            PORTB |=_BV(6);
+            PORTE &= ~_BV(2);
         break;
         case 7:    //1, 1, 1, 0
-        PORTB |= _BV(5);
-        PORTD |= _BV(4);
-        PORTB |=_BV(6);
-        PORTE &= ~_BV(2);
+            PORTB |= _BV(5);
+            PORTD |= _BV(4);
+            PORTB |=_BV(6);
+            PORTE &= ~_BV(2);
         break;
       }
-    
     }
-
 }
 
-
-
-void uPinMode(uint8_t pin, uint8_t mode)
-{
+void uPinMode(uint8_t pin, uint8_t mode) {
     STRELA_INIT_CHECK;
         
-    if (pin < NUM_DIGITAL_PINS) // standart IO
-        {
+    if (pin < NUM_DIGITAL_PINS) { // standart IO
             pinMode( pin, mode );
             return;
         }
-        
-      
-    if ( pin > Q7)  //we can,t change pinMode on GPUX on addr 0x20 or more
-    {
+
+    if ( pin > Q7) {  //we can,t change pinMode on GPUX on addr 0x20 or more
         return;
-    }
-    else 
-    {
+    } else  {
         if (mode != OUTPUT)
             lcdPinMode |= 1 << ( _LC(pin) );
         else
-            lcdPinMode &= ~(1 << ( _LC(pin) ));       
-         
+            lcdPinMode &= ~(1 << ( _LC(pin) ));
             twiPinMode(LCD_TWI_ADDR, lcdPinMode);
     }                
 }
 
-
-void uDigitalWrite(uint8_t pin, uint8_t val)
-{
+void uDigitalWrite(uint8_t pin, uint8_t val) {
     STRELA_INIT_CHECK;
         
-    if (pin < NUM_DIGITAL_PINS) // standart IO
-    {
+    if (pin < NUM_DIGITAL_PINS) { // standart IO 
         digitalWrite( pin, val );
         return;
     }
     
-    if ( pin > LAST_PIN)  //NOT ON PIN
-    {
-        if ((pin > L1-1) && (pin < L4+1)) // it is LED?
-        {
+    if ( pin > LAST_PIN) {                                                      //NOT ON PIN
+        if ((pin > L1-1) && (pin < L4+1)) {                                     // it is LED?
             uint8_t ld = pin - L1;
             (val) ? ledState |= 1 << (7 - ld) : ledState &= ~(1 << (7 - ld));
-            twiWriteOut(GPUX_TWI_ADDR, ~ledState);   // LED is lighting on 0
+            twiWriteOut(GPUX_TWI_ADDR, ~ledState);                              // LED is lighting on 0
         }
         return;
     }
     
-    if ( pin > Q7)  //led or button
-    {
-        if ( pin > LS3 ) // only LED
-        {
+    if ( pin > Q7) {                                                        //led or button
+        if ( pin > LS3 ) {                                                  // only LED
             uint8_t curPin = pin - LS0;
             (val) ? ledState |= 1 << curPin : ledState &= ~(1 << curPin);
-            twiWriteOut(GPUX_TWI_ADDR, ~ledState); // LED is lighting on 0
+            twiWriteOut(GPUX_TWI_ADDR, ~ledState);                          // LED is lighting on 0
         }
         return;
-    }   
-    
-    else // pin>=Q0 && pin<=Q7
-    {
+    } else { // pin>=Q0 && pin<=Q7
         uint8_t curPin = _LC(pin);
         (val) ? lcdPinState |= 1 << curPin : lcdPinState &= ~(1 << curPin);
         twiWriteOut(LCD_TWI_ADDR, lcdPinState);
     }        
 }
 
-uint8_t uDigitalRead(uint8_t pin)
-{
+uint8_t uDigitalRead(uint8_t pin) {
     STRELA_INIT_CHECK;
         
-    if (pin < NUM_DIGITAL_PINS) // standart IO
+    if (pin < NUM_DIGITAL_PINS)                                 // standart IO
         return digitalRead( pin );
     
-    if ( pin > LAST_PIN)  //NOT ON PIN
-    {
-        if ((pin > S1-1) && (pin < S4+1)) // it is Button?
+    if ( pin > LAST_PIN) {                                      //NOT ON PIN
+        if ((pin > S1-1) && (pin < S4+1))                       // it is Button?
             return !twiReadIn(GPUX_TWI_ADDR, (3 - (pin - S1))); // !, pressed button return 0, need 1
-            
+
         else return LOW;
     }
     
-    if ( pin > Q7)  //led or button
-    {
+    if ( pin > Q7) {                                //led or button
         uint8_t curPin = pin - LS0;
-        return !twiReadIn(GPUX_TWI_ADDR, curPin); // !, pressed button return 0, need 1
-            
-    }   
-    
-    else // pin>=Q0 && pin<=Q7
-    {
+        return !twiReadIn(GPUX_TWI_ADDR, curPin);   // !, pressed button return 0, need 1
+    } else {                                        // pin>=Q0 && pin<=Q7
         uint8_t curPin = pin - Q0;
         return twiReadIn(LCD_TWI_ADDR, curPin);
     }        
 }
 
-int uAnalogRead(uint8_t pin)
-{
+int uAnalogRead(uint8_t pin) {
     return analogRead(pin);
 }
 
-void uAnalogWrite(uint8_t pin, int val)
-{
+void uAnalogWrite(uint8_t pin, int val) {
     analogWrite(pin, val);
 }
 
 
-void strelaInitialize()
-{
+void strelaInitialize() {
    // Configure I2C i/o chip
     twiPinMode(GPUX_TWI_ADDR, WIRE_IO_CONFIGURATION);
-    
+
     // Configure I2C LCD i/o chip to OUTPUT
     twiPinMode(LCD_TWI_ADDR, 0xff);
 
-    
     // Configure Pin
-
     DDRE |= _BV(2);           //pinMode(PE2, OUTPUT);
     DDRD |= _BV(4);           //pinMode(4, OUTPUT);
     DDRB |= _BV(5) | _BV(6);  //pinMode(9,10, OUTPUT);
-    
-    
+
     // Configure software PWM4D Timer to fast PWM
     TCCR4A |= (1<<COM4B1); //enable communicate pin 10 and OCR4
     TCCR4D &= ~(1<<WGM41) & ~(1<<WGM40); 
     TC4H =0;
     
     strelaInit = true;
-    
 } 
 
 
-void motorConnection(
-            bool direction_1,
-            bool direction_2)
-{
+void motorConnection( bool direction_1, bool direction_2) {
     STRELA_INIT_CHECK;
         
     _motorConnection_1 = direction_1;
     _motorConnection_2 = direction_2;    
 }
 
-void motorSpeed(uint8_t motor, int speed)
-{
+void motorSpeed(uint8_t motor, int speed) {
     STRELA_INIT_CHECK;
         
     _setMotorDirection(motor, (speed < 0));  
 
-    if (motor = M1)
+    if (motor == M1)
         _setMotorSpeed_1(speed);
-    else if (motor = M2)
+    else if (motor == M2)
         _setMotorSpeed_2(speed);
-        
 }
 
-void drive(
-            int motorSpeed_1,
-            int motorSpeed_2)
-{ 
+void drive( int motorSpeed_1, int motorSpeed_2) {
     STRELA_INIT_CHECK;
         
     _setMotorDirections((motorSpeed_1 < 0), (motorSpeed_2 < 0));
@@ -427,42 +346,39 @@ void drive(
     
     //Motor2 PWM is hardware 
     _setMotorSpeed_2(motorSpeed_2);
-
 }
 
 
-void stepperMotor(int stepsToMove, int stepDelay) 
-{
+void stepperMotor(int stepsToMove, int stepDelay) {
     STRELA_INIT_CHECK;
 
-  motorSpeed(0,0);
+    motorSpeed(0,0);
 
-  int stepsLeft = abs(stepsToMove);  // how many steps to take
+    int stepsLeft = abs(stepsToMove);  // how many steps to take
   
-  static uint8_t stepNumber = 0;
+    static uint8_t stepNumber = 0;
 
-  unsigned long lastStepTime = 0;
+    unsigned long lastStepTime = 0;
 
-  // decrement the number of steps, moving one step each time:
-  while(stepsLeft > 0) {
+    // decrement the number of steps, moving one step each time:
+    while(stepsLeft > 0) {
 
     // move only if the appropriate delay has passed:
     if (millis() - lastStepTime >= stepDelay) {
-      // get the timeStamp of when you stepped:
-      lastStepTime = millis();
-      // increment or decrement the step number,
-      // depending on direction:
-      if (stepsToMove > 0) 
-        stepNumber++;      
-      else       
-        stepNumber--;
-      
-      // decrement the steps left:
-      stepsLeft--;
+        // get the timeStamp of when you stepped:
+        lastStepTime = millis();
+        // increment or decrement the step number,
+        // depending on direction:
+        if (stepsToMove > 0) 
+            stepNumber++;      
+        else
+            stepNumber--;
+
+        // decrement the steps left:
+        stepsLeft--;
 
         // step the motor to step number 0, 1, 2, 3, 4, 5, 6, 7, 8:
         _stepMotor(stepNumber %= 8);
     }
   }
-
 }
